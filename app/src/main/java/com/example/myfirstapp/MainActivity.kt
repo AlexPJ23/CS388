fad749d9735a5529243e215d4d43c239b29ac1c8
Alex Joseph <AlexPJoseph80@gmail.com>
2024-01-24 15:27:43 -0500

    Completed the prework entirely

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.PressMebutton)
+        button.setOnClickListener{
+            Log.v("Hello World", "Button Clicked")
+            Toast.makeText(this, "Hello to you to", Toast.LENGTH_SHORT).show()
+        }
     }
 }
