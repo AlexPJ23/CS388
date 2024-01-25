a7823fc2d4257279a03598d27c5d0ca1fda6324a
Alex Joseph <AlexPJoseph80@gmail.com>
2024-01-25 12:37:15 -0500

    Added functionality to button and changed the font size of the text

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,22 +2,37 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var counter = 0
+     var counter : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.addBtn)
+        val upgradeBtn = findViewById<Button>(R.id.upgradeBtn)
         val textView = findViewById<TextView>(R.id.textView)
         button.setOnClickListener{
             counter++
             textView.text = counter.toString()
             Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT)
+            if ( counter >= 100 ){
+                upgradeBtn.visibility = View.VISIBLE
+                upgradeBtn.setOnClickListener {
+                    button.text="Add 2"
+                    button.setOnClickListener {
+                        counter+=2
+                        textView.text = counter.toString()
+                    }
+                    upgradeBtn.visibility = View.INVISIBLE
+                }
+
+            }
         }
+
     }
 
 }
