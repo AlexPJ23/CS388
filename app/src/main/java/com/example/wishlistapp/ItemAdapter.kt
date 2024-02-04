ddcf06682913766f431c69f8a327999f732861a1
Alex Joseph <AlexPJoseph80@gmail.com>
2024-02-04 18:45:12 -0500

    Finished with adapter and figuring out how to have it respond to the frontend

--- app/src/main/java/com/example/wishlistapp/ItemAdapter.kt
+++ app/src/main/java/com/example/wishlistapp/ItemAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.wishlistapp
 
-class ItemAdapter {
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+import android.view.View
+import android.view.LayoutInflater
+
+class ItemAdapter (private val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+        val item : TextView
+        val price: TextView
+        val store: TextView
+
+        init {
+
+            item = itemView.findViewById(R.id.item)
+            price = itemView.findViewById(R.id.price)
+            store = itemView.findViewById(R.id.store)
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
+        val context =  parent.context
+        val inflator = LayoutInflater.from(context)
+        val contactVeiw = inflator.inflate(R.layout.item_layout,parent,false)
+        return ViewHolder(contactVeiw)
+    }
+
+    override fun onBindViewHolder(holder: ItemAdapter.ViewHolder, position: Int) {
+        val items = items[position]
+        holder.item.text = items.name
+        holder.price.text = items.price.toString()
+        holder.store.text = items.url
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
 }
--- app/src/main/java/com/example/wishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/wishlistapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.wishlistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val itemRv = findViewById<RecyclerView>(R.id.recyclerView)
+        val items : MutableList<Item> = ArrayList<Item>()
+        val adapter : ItemAdapter = ItemAdapter(items)
+        itemRv.adapter = adapter
+        itemRv.layoutManager = LinearLayoutManager(this)
+        findViewById<Button>(R.id.button2).setOnClickListener {
+
+            adapter.notifyDataSetChanged()
+        }
+
     }
 }
