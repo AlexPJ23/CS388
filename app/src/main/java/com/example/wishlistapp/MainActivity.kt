223518553bb7e5e881da74318f813aafde8f8964
Alex Joseph <AlexPJoseph80@gmail.com>
2024-02-04 20:15:30 -0500

    Attempt Finish 1

--- app/src/main/java/com/example/wishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/wishlistapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wishlistapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,10 +14,16 @@ class MainActivity : AppCompatActivity() {
         val itemRv = findViewById<RecyclerView>(R.id.recyclerView)
         val items : MutableList<Item> = ArrayList<Item>()
         val adapter : ItemAdapter = ItemAdapter(items)
+        val data = arrayListOf<EditText>(
+            findViewById(R.id.itemName),
+            findViewById(R.id.price),
+            findViewById(R.id.source)
+        )
         itemRv.adapter = adapter
         itemRv.layoutManager = LinearLayoutManager(this)
         findViewById<Button>(R.id.button2).setOnClickListener {
-
+            var new_item : Item = Item(data[0].text.toString(),data[1].text.toString().toFloat(),data[2].text.toString())
+            items.add(new_item)
             adapter.notifyDataSetChanged()
         }
 
